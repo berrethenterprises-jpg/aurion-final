cd55176e8828ab039c0816e9ef98ee836a9f8348
berrethenterprises-jpg <berrethenterprises@gmail.com>
2026-03-29 18:36:16 -0700

    Add files via upload

---
+++ core/market/price.js
@@ -0,0 +1,18 @@
+﻿const axios=require('axios')
+
+async function price(token){
+
+try{
+
+const r=await axios.get('https://api.dexscreener.com/latest/dex/tokens/'+token)
+return r.data.pairs?.[0]
+
+}catch{
+
+return null
+
+}
+
+}
+
+module.exports={price}
---
+++ core/shadow/engine.js
@@ -0,0 +1,25 @@
+﻿const exec=require('../execution/exec')
+const price=require('../market/price')
+const heat=require('../liquidityHeatmap/heatmap')
+
+async function run(){
+
+const tokens=['So11111111111111111111111111111111111111112']
+
+for(const t of tokens){
+
+const m=await price.price(t)
+
+if(!m) continue
+
+heat.update(t,m.liquidity?.usd||0)
+
+if(!heat.hot(t)) continue
+
+exec.exec(t,m)
+
+}
+
+}
+
+module.exports={run}
